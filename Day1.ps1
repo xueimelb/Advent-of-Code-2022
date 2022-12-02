7e743d77ecd0e8dd507170a9f1585175ae4c49e4
xueimelb <55091688+xueimelb@users.noreply.github.com>
2022-12-02 10:50:19 -0600

    Update Day1.ps1
    making it more human readable

--- Day1.ps1
+++ Day1.ps1
@@ -1,10 +1,13 @@
-$input = Get-Content C:\AOC\input
+$input = Get-Content C:\AOC\Day01input
 $totals = [System.Collections.ArrayList]@()
 $calories = 0
 foreach ($item in $input) {
-    if ($item -gt 0) {$item} else {$totals.Add($calories); $calories = 0}
+    if ($item -gt 0) {$item | Out-Null} else {$totals.Add($calories) | Out-Null; $calories = 0}
     $calories += $item
-    if ($Item -eq $input[-1]) {$totals.Add($calories)}
+    if ($Item -eq $input[-1]) {$totals.Add($calories) | Out-Null}
 }
 $totalsSorted = $totals | Sort-Object -Descending
 $top = $totalsSorted[0] + $totalsSorted[1] + $totalsSorted[2]
+
+Write-Host "Answer for part 1 is "$totalsSorted[0]
+Write-Host "Answer for part 2 is $top"
